620bf079354802b4bb8952f7e28c1c46aee5f5e0
smsutherland <sagan@farthinghalearms.com>
2021-10-30 21:00:51 -0400

    Flattening images

--- SingleFrameReducer.py
+++ SingleFrameReducer.py
@@ -12,6 +12,7 @@ class SingleFrameReducer:
 
     _bias_frame: Image.Image = None
     _dark_current_frame: Image.Image = None
+    _normalized_flat_frame: Image.Image = None
     
     def set_bias_frames(self, file_name: Union[str, List[str]]) -> None:
         """
@@ -57,6 +58,32 @@ class SingleFrameReducer:
             frame = frame/exposure_time
         self._dark_current_frame = np.mean(dark_current_frames_bias_subtracted, axis=0)
     
+    def set_flat_frames(self, file_name: Union[str, List[str]]) -> None:
+        """
+        TODO
+        """
+
+        if self._bias_frame is None:
+            raise Exception("call set_bias_frames before calling this function")
+        if self._dark_current_frame is None:
+            raise Exception("call set_dark_current_frames before calling this function")
+
+        file_name_list = []
+        if isinstance(file_name, list):
+            for fname in file_name:
+                file_name += glob(fname)
+        else:
+            file_name_list = glob(file_name)
+        
+        if len(file_name_list) == 0:
+            raise Exception("No files specified or file not found.")
+        
+        flat_frames_full = [Image.from_file(fname, True) for fname in file_name_list]
+        flat_frames_dark_subtracted = [self.dark_subtract(frame[0], frame[1]["EXPTIME"]) for frame in flat_frames_full]
+        mean_flat_frame = np.mean(flat_frames_dark_subtracted, axis=0)
+        self._normalized_flat_frame = mean_flat_frame/np.mean(mean_flat_frame)
+
+    
     def bias_subtract(self, frame: Image.Image) -> Image.Image:
         """
         TODO
@@ -72,12 +99,26 @@ class SingleFrameReducer:
         TODO
         """
 
-        if self._dark_current_frame is None:
-            raise Exception("call set_dark_current_frames before dark subtracting")
         if self._bias_frame is None:
             raise Exception("call set_bias_frames before bias subtracting")
+        if self._dark_current_frame is None:
+            raise Exception("call set_dark_current_frames before dark subtracting")
         
-        return frame - (self._bias_frame + exposure_time*self._dark_current_frame)
+        return self.bias_subtract(frame) - exposure_time*self._dark_current_frame
+    
+    def flatten(self, frame: Image.Image, exposure_time: float) -> Image.Image:
+        """
+        TODO
+        """
+
+        if self._bias_frame is None:
+            raise Exception("call set_bias_frames before flattening")
+        if self._dark_current_frame is None:
+            raise Exception("call set_dark_current_frames before flattening")
+        if self._normalized_flat_frame is None:
+            raise Exception("set set_flat_frames before flattening")
+        
+        return self.dark_subtract(frame, exposure_time)/self._normalized_flat_frame
 
     def bias_frame(self) -> Image.Image:
         """
